ceabfdc0306ff2ddc495ca7c5320a02fbd94dae2
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-02-19 13:16:11 -0800

    re setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
-import history from '../history';
-// import Header from './Header';
 
 const App = () => {
     return <div>App</div>;
-// 	return (
-// 		<div className="ui container">
-// 			<Router history={history}>
-// 				<div>
-// 					<Header />
-// 					<Switch>
-// 						<Route path="" />
-// 					</Switch>
-// 				</div>
-// 			</Router>
-// 		</div>
-// 	);
-// };
 }
 
+
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import reducers from './reducers';
 
 
 import App from './components/App';
