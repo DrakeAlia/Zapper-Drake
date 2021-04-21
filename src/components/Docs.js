86af3bbfedfd0c1c759cbb2fd6483a8e1163127f
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-04-20 23:52:03 -0700

    Routes 4

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -28,13 +28,45 @@ const Docs = () => {
 		<Documentation>
 			<DocsHeader>
 				Introduction
-            
-			</DocsHeader>
-			<DocsNav>
+			</DocsHeader> 
+			<DocsNav> <br></br>
+            Introduction <br></br>
+                API <br></br>
+                Getting Started <br></br>
+                Endpoints <br></br>
+                API Guides <br></br>
+                Token Balances <br></br>
+                Supported Tokens and Prices <br></br>
+                Gas Prices <br></br>
+                Protocol Balances <br></br>
+                Pool Stats <br></br>
+                Vault Stats <br></br>
+                Zap In <br></br>
+                Zap Out <br></br>
+                Smart Contracts <br></br>
+                Learn <br></br> 
+                DashBoard <br></br>
+                Exchange <br></br>
+                Pool <br></br>
+                Bridge <br></br>
+                Farm <br></br>
+                Additional Resources <br></br>
+                Brand Assets <br></br>
+                Join Our Discord <br></br>
+                Tutorials FAQs <br></br>
+                Feature Requests <br></br>
             </DocsNav>
             <DocsBody>
+                Making DeFi easy to use
+                <DocsFeatures>Features</DocsFeatures>
+                <DocsZapperApi>1. Zapper API</DocsZapperApi>
+                <DocsSmartContracts>2. Smart Contracts</DocsSmartContracts>
+                <DocsLearnZapper>3. Learn Zapper</DocsLearnZapper>
+                If you have questions or would like to learn more to about Zapper feel free to join our Discord
+                <DocsIndex>Getting Started</DocsIndex>
             </DocsBody>
             <DocsFooter>
+                WAS THIS PAGE HELPFUL?
             </DocsFooter>
 		</Documentation>
 	);
@@ -52,6 +84,21 @@ const DocsNav = styled.div`
 `;
 
 const DocsBody= styled.div`
+`;
+
+const DocsFeatures= styled.div`
+`;
+
+const DocsZapperApi = styled.div`
+`;
+
+const DocsSmartContracts = styled.div`
+`;
+
+const DocsLearnZapper = styled.div`
+`;
+
+const DocsIndex = styled.div`
 `;
 
 const DocsFooter = styled.div`
